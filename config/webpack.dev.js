3f662a864c31f23f29860d9f673539f1d7d9a049
byoungyoon <bur5698@naver.com>
2022-06-14 10:51:15 +0900

    webpack 적용

---
+++ config/webpack.dev.js
@@ -0,0 +1,27 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {
+    open: false,
+    hot: true,
+    compress: true,
+    port: 8001,
+    historyApiFallback: true,
+    liveReload: true,
+  },
+  output: {
+    filename: '[name].min.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+});
